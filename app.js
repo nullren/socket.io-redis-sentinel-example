c7303f3e4705eab69383908a9b4fae677a725b3d
renning bruns <rbruns@ixl.com>
2015-04-21 14:00:44 -0700

    sentinel works!

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var app           = require('express')();
 var http          = require('http').Server(app);
 var io            = require('socket.io')(http);
+var ra            = require('socket.io-redis');
+var redis         = require('redis-sentinel').createClient;
 
 var PORT = process.env.PORT || 30000;
 
 app.get('/', function(req, res) {
   res.sendfile("index.html");
 });
+
+var SENTINELS = [
+    {host: '127.0.0.1', port: 23460},
+    {host: '127.0.0.1', port: 23461},
+    {host: '127.0.0.1', port: 23462}
+  ];
+
+var pub = redis(SENTINELS, 'mymaster', { detect_buffers: true })
+var sub = redis(SENTINELS, 'mymaster', { detect_buffers: true })
+
+io.adapter(ra({ pubClient: pub, subClient: sub }));
 
 io.on('connection', function(socket) {
   console.log(socket.id);
